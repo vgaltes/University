eab349fbf6928f5cbb6d0defcba5c6b648c20709
Vicenc Garcia-Altes <vicentredrum@gmail.com>
2018-12-03 11:54:45 +0000

    Initial preparations for integration tests

--- tests/steps/when.js
+++ tests/steps/when.js
@@ -1,13 +1,13 @@
 const http = require("superagent-promise")(require("superagent"), Promise);
+const mode = process.env.TEST_MODE;
 
-module.exports.we_invoke_get_masters = async () => {
-  const apiRoot = "https://6rf3v00hv9.execute-api.eu-west-1.amazonaws.com/dev";
-  const functionPath = "masters";
+async function viaHttp(functionPath, method) {
+  const apiRoot = process.env.TEST_BASE_URL;
 
   const url = `${apiRoot}/${functionPath}`;
 
   try {
-    const res = await http("GET", url);
+    const res = await http(method, url);
 
     return {
       statusCode: res.status,
@@ -21,4 +21,13 @@ module.exports.we_invoke_get_masters = async () => {
     }
     throw err;
   }
+}
+
+async function viaHandler(functionPath) {
+  const handler = require(`../../src/functions/${functionPath}`);
+  return await handler.handler();
+}
+
+module.exports.we_invoke_get_masters = () => {
+  return mode === "http" ? viaHttp("masters", "GET") : viaHandler("getMasters");
 };
